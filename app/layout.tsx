262bb7af4a4842d01c9072e07f507c66317bda4d
Raul Sigoli <raul_sigoli@hotmail.com>
2024-04-17 13:55:23 -0300

    fix: ajustando o projeto

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Raul - Sigoli",
+  description: "Curriculo Profissional do Raul Sigoli",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
